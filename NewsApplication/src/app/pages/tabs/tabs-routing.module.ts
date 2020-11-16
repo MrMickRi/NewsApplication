9dcbb174dceacb59cdeb7f5ed5f11fad3fc5bd0b
Mick Murphy <mickmurphy1993@hotmail.com>
2020-11-16 19:27:58 +0000

    created routing tab visuals + homepage title

--- NewsApplication/src/app/pages/tabs/tabs-routing.module.ts
+++ NewsApplication/src/app/pages/tabs/tabs-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
                 ]
             },
             {
-                path: 'buisness',
+                path: 'business',
                 children: [
                     {
                         path: '',
