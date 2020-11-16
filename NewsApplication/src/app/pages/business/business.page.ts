2836882269f9d13ae5ca6f5eed59a330f5649710
Mick Murphy <mickmurphy1993@hotmail.com>
2020-11-16 22:47:28 +0000

    APIChanges

--- NewsApplication/src/app/pages/business/business.page.ts
+++ NewsApplication/src/app/pages/business/business.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 
 @Component({
-  selector: 'app-business',
-  templateUrl: './business.page.html',
-  styleUrls: ['./business.page.scss'],
+    selector: 'app-business',
+    templateUrl: './business.page.html',
+    styleUrls: ['./business.page.scss'],
 })
 export class BusinessPage implements OnInit {
+    businesses: Observable<any>;
+    
+    constructor(private router: Router, private api: ApiService) { }
 
-  constructor() { }
+    ngOnInit() {
+        console.log('Starting Call');
+        this.businesses = this.api.getBusinessNews();
+        console.log(this.businesses);
+        this.businesses.subscribe(data => {
+            console.log('My Data: ', data);
+        });
+    }
 
-  ngOnInit() {
-  }
+    openArticle(business) {
+        let businessID = business.business_id;
+        this.router.navigateByUrl('/tabs/business/${businessID}')
+    }
 
 }
---
+++ NewsApplication/src/app/services/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService {
+    apiKey = 'eXWdJJcGlKPjikvqecBSKAQd6ZTjrDNW';
+
+    constructor(private http: HttpClient) { }
+
+    getBusinessNews() {
+        return this.http.get('https://content.guardianapis.com/search?q=business&api-key=a007ac0a-881b-4ef1-8626-d6177f076104')
+    }
+    getArticle(id) {
+        return this.http.get('https://content.guardianapis.com/${id}?api-key=a007ac0a-881b-4ef1-8626-d6177f076104')
+    }
+
+}
