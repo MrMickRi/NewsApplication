0307b136a3976451b587c6718f8390af832f4511
Mick Murphy <mickmurphy1993@hotmail.com>
2020-11-16 19:08:29 +0000

    Changed routing Paths & removed initial Homepage

--- NewsApplication/src/app/pages/tabs/tabs-routing.module.ts
+++ NewsApplication/src/app/pages/tabs/tabs-routing.module.ts
@@ -4,14 +4,69 @@ import { Routes, RouterModule } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: TabsPage
-  }
+    {
+        path: 'tabs',
+        component: TabsPage,
+        children: [
+            {
+                path: 'homepage',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../homepage/homepage.module').then(m => m.HomepagePageModule)
+                    }
+                ]
+            },
+            {
+                path: 'buisness',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../business/business.module').then(m => m.BusinessPageModule)
+                    },
+                    {
+                        path: ':id',
+                        loadChildren: () => import('../business-details/business-details.module').then(m => m.BusinessDetailsPageModule)
+                    }
+                ]
+            },
+            {
+                path: 'sports',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../sports/sports.module').then(m => m.SportsPageModule)
+                    },
+                    {
+                        path: ':id',
+                        loadChildren: () => import('../sports-details/sports-details.module').then(m => m.SportsDetailsPageModule)
+                    }
+                ]
+            },
+            {
+                path: 'finance',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../finance/finance.module').then(m => m.FinancePageModule)
+                    },
+                    {
+                        path: ':id',
+                        loadChildren: () => import('../finance-details/finance-details.module').then(m => m.FinanceDetailsPageModule)
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        path:'',
+        redirectTo: '/tabs/homepage',
+        pathMatch:  'full'
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule { }
